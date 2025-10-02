b0ae9f766f45fabbdb6cddbd2010d61b47204acd
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-10-02 21:45:12 +0200

    added usestate

---
+++ src/pages/Product.jsx
@@ -0,0 +1,85 @@
+import delivery from "../assets/images/delivery.svg";
+import car from "../assets/images/car.svg";
+import homelove from "../assets/images/homelove.svg";
+import product from "../assets/images/product.jpg";
+import { useOutletContext, useParams } from "react-router-dom";
+
+export const Product = () => {
+  const { products } = useOutletContext();
+  const { id } = useParams();
+  const findProduct = products.find((p) => p.id === +id);
+
+  return (
+    <section className="content">
+      <div className="container">
+        {findProduct ? (
+          <div className="content-box">
+            <div className="content-product">
+              <div className="content-product__left">
+                <h2 className="content-product__title">{findProduct.title}</h2>
+                <img src={product} alt="product" />
+                <p className="content-product__text">
+                  {findProduct.description}
+                </p>
+              </div>
+              <div className="content-product__right">
+                <h2 className="content-product__price">
+                  {findProduct.price} ₽
+                </h2>
+                <button className="btn btn-primary btn-large">
+                  Показать телефон
+                </button>
+              </div>
+            </div>
+            <div className="content-side">
+              <h3 className="content-side__title">Сервисы и услуги</h3>
+              <div className="content-side__box">
+                <div className="content-side__list">
+                  <div className="content-side-item">
+                    <img src={delivery} alt="delivery" />
+                    <h5 className="content-side-item--title">Доставка</h5>
+                    <p className="content-side-item--text">
+                      Проверка при получении и возможность бесплатно вернуть
+                      товар
+                    </p>
+                  </div>
+                  <div className="content-side-item">
+                    <img src={car} alt="car" />
+                    <h5 className="content-side-item--title">Автотека</h5>
+                    <p className="content-side-item--text">
+                      Отчёт с историей авто: пробег, владельцы, сведения о
+                      залоге, ДТП и ремонтах
+                    </p>
+                  </div>
+                  <div className="content-side-item">
+                    <img src={homelove} alt="homelove" />
+                    <h5 className="content-side-item--title">
+                      Онлайн-бронирование жилья
+                    </h5>
+                    <p className="content-side-item--text">
+                      Посуточная аренда квартир и домов: большой выбор вариантов
+                      для поездок по России
+                    </p>
+                  </div>
+                </div>
+                <div className="content-side__footer">
+                  <p className="content-side__footer--item">
+                    © ООО «Абито», 2011–2021
+                  </p>
+                  <a href="#!" className="content-side__footer--item">
+                    Политика конфиденциальности
+                  </a>
+                  <a href="#!" className="content-side__footer--item">
+                    Обработка данных
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <h2>Такого товара не существует</h2>
+        )}
+      </div>
+    </section>
+  );
+};
